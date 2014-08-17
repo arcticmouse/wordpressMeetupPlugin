4aef3a8340cb4dcbaf2d5e14ba4346babfc6de68
arcticmouse <me@notyetleta.net>
2014-08-17 17:15:44 -0500

    Update wordpressMeetupPlugin.php

--- wordpressMeetupPlugin.php
+++ wordpressMeetupPlugin.php
@@ -38,7 +38,7 @@ add_action( 'wp_head', 'add_my_google_analytics_code_to_header' );
 function add_my_google_analytics_code_to_header() {
 	//$ga_code = "UA-46249838-1";
 	$ga_code = get_option( 'ga_code_options_text' );
-	if ( $ga_code ){
+	if ( $ga_code && ( $ga_code != "") ){
 		echo "<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 ga('create', '$ga_code', 'auto');
 ga('send', 'pageview');
